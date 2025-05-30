e70d39e7005eea772a24435f2809b6d5f8fa1aee
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-05-29 21:26:54 -0400

    light changes to displaying movies

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 interface Movie {
     id: string;
     title: string;
@@ -10,9 +14,12 @@ interface MovieCardProps {
 }
   
 export default function MovieCard({ movie }: MovieCardProps) {
+    const [expandedView, setExpandedView] = useState(false);
+
     return (
         <div 
             className="flex flex-col p-4 hover:cursor-pointer transform transition-transform duration-200 hover:scale-110"
+            onClick={() => setExpandedView(true)}
         >
             <img src={movie.poster} alt={movie.title} className="w-full h-auto mb-2 rounded-lg" />
             <h2 className="text-lg font-semibold">{movie.title}</h2>
--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -43,8 +43,6 @@ export default function MovieGrid() {
         fetchMovies();
     }, []);
 
-    console.log(movies);
-
     return (
         <section>
             <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
