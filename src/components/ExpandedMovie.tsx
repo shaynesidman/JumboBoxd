2b974c3109e2d2f512f11bd1267e522d9bdae963
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-06-06 10:26:36 -0400

    added laoding screen to expanded movie to avoid template/boilerplate code from showing before data is loaded

--- src/components/ExpandedMovie.tsx
+++ src/components/ExpandedMovie.tsx
@@ -15,7 +15,8 @@ type Movie = {
 }
   
 export default function ExpandedMovie({ movieID }: ExpandedMovieProps) {
-    const [movie, setMovie] = useState<Movie | null>(null)
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState<Movie | null>(null);
 
     useEffect(() => {
         async function fetchMovie() {
@@ -26,6 +27,7 @@ export default function ExpandedMovie({ movieID }: ExpandedMovieProps) {
 
                 const movieInfo = await res.json();
                 setMovie(movieInfo);
+                setLoading(false);
             } catch (error) {
                 console.error("Error fetching movies: ", error);
                 setMovie(null);
@@ -35,31 +37,39 @@ export default function ExpandedMovie({ movieID }: ExpandedMovieProps) {
         fetchMovie();
     }, []);
 
-    return (
-        <div className="flex flex-col md:flex-row gap-6">
-            <img
-                src={movie?.poster}
-                alt={movie?.title}
-                className="w-full md:w-1/2 h-auto object-contain rounded-lg shadow"
-            />
-            <div className="flex flex-col gap-4">
-                <h1><span className="font-bold">{movie?.title}</span> ({movie?.year})</h1>
-                <p>{movie?.description}</p>
-                <div className="flex flex-row justify-between">
-                    <button className="bg-white shadow-sm hover:shadow-lg text-black rounded-md py-1 px-2">
-                        Haven't seen
-                    </button>
-                    
-                    <div className="flex flex-row gap-2">
+    if (!loading) {
+        return (
+            <div className="flex flex-col md:flex-row gap-6">
+                <img
+                    src={movie?.poster}
+                    alt={movie?.title}
+                    className="w-full md:w-1/2 h-auto object-contain rounded-lg shadow"
+                />
+                <div className="flex flex-col gap-4">
+                    <h1><span className="font-bold">{movie?.title}</span> ({movie?.year})</h1>
+                    <p>{movie?.description}</p>
+                    <div className="flex flex-row justify-between">
                         <button className="bg-white shadow-sm hover:shadow-lg text-black rounded-md py-1 px-2">
-                            Like
+                            Haven't seen
                         </button>
-                        <button className="bg-white shadow-sm hover:shadow-lg text-black rounded-md py-1 px-2">
-                            Dislike
-                        </button>
+                        
+                        <div className="flex flex-row gap-2">
+                            <button className="bg-white shadow-sm hover:shadow-lg text-black rounded-md py-1 px-2">
+                                Like
+                            </button>
+                            <button className="bg-white shadow-sm hover:shadow-lg text-black rounded-md py-1 px-2">
+                                Dislike
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+        );
+    }
+
+    return (
+        <div className="flex justify-center items-center h-full w-full">
+            <div className="animate-spin rounded-full h-12 w-12 border-4 border-t-transparent border-black"></div>
         </div>
     );
 }
